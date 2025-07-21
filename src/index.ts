10764af7c16723925c3d4a32d22b5aa6de140ecd
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-07-21 12:49:13 +0200

    feat: add security middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { secureHeaders } from "hono/secure-headers";
+import { bodyLimitMiddleware } from "./lib/body-limit.ts";
+import { corsMiddleware } from "./lib/cors.ts";
 import { ConfigSchema } from "./schemas/config.schema.ts";
 
 const config = ConfigSchema.parse(process.env);
 
 const app = new Hono();
+
+app.use(secureHeaders());
+app.use(bodyLimitMiddleware());
+app.use(corsMiddleware(config));
 
 app.get("/", (c) => {
   return c.text("Hello, World!");
--- src/schemas/config.schema.ts
+++ src/schemas/config.schema.ts
@@ -4,6 +4,7 @@ export const ConfigSchema = z.object({
   HOSTNAME: z.string().default("localhost"),
   PORT: z.coerce.number().min(1).max(65535),
   POSTGRES_URL: z.string(),
+  CORS_ORIGINS: z.string(),
 });
 
 export type Config = z.infer<typeof ConfigSchema>;
